7bb085cd84903f1162696478e8f7cedaf3da4bf6
KadEyy <k.cstmz@gmail.com>
2020-05-22 13:22:30 +0200

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './index.css';
+
+ReactDOM.render(
+    <div>React TS Template</div>,
+  document.getElementById('root') as HTMLElement
+);
